2bbe50148a2464a382007aa4e37a88f320f5e628
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-08 15:01:40 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -17,8 +17,9 @@ def piglatinize(text)
         @new.push(@latin_vowel)
     else
        @c = x.scan(/\w/)
-       2.times do
        @c.insert(-1, @c.delete_at(0))
+          @c.insert(-1, @c.delete_at(0)) if !@c[0].match(/\b[AEIOUaeiou][a-z]*\b/)
+        
        end
        @latin_cons = "#{@c.join("")}ay"
        @new.push(@latin_cons)
