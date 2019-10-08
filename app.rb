ec684afaa25b956baf76a7987859c012854ed692
Michael McCullough <michael.mccullough21@gmail.com>
2019-10-08 15:16:07 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,8 +8,7 @@ class App < Sinatra::Base
   end
 
   post '/piglatinize' do
-    @pigtext = PigLatinizer.new
-    @pigtext.piglatinize(params[:user_phrase])
+    @pigtext = PigLatinizer.new(params[:user_phrase])
     erb :piglatinize
   end
 end
